4688536d64ce95a0856ac5159a7c0023ccaa5be4
Erik R Sánchez Pérez <erik.sape@gmail.com>
2022-02-25 00:10:04 -0600

    feat: added the zip output

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from typing import Optional
 import requests
+import zipfile
+from io import BytesIO
+from datetime import datetime
 from fastapi import FastAPI
+from fastapi.responses import StreamingResponse
 
 app = FastAPI()
 
@@ -10,7 +14,7 @@ def read_root():
 
 
 @app.get("/berries")
-def read_item(name: Optional[str] = None, limit: Optional[int] = 10, offset: Optional[int] = 0):
+def read_item(name: Optional[str] = None, limit: Optional[int] = 10, offset: Optional[int] = 0, output: Optional[str] = 'json'):
     name_filter = ""
     if name:
         name_filter = ',where: {name: {_in: [%s]}}' % (name)
@@ -28,6 +32,24 @@ def read_item(name: Optional[str] = None, limit: Optional[int] = 10, offset: Opt
     r = requests.post('https://beta.pokeapi.co/graphql/v1beta', json={"query": query})
 
 
-    return {"data": r.json()['data'], "limit": limit, "offset": offset, "next_offset": limit + offset}
+    if output == 'json':
 
+        return {"data": r.json()['data'], "limit": limit, "offset": offset, "next_offset": limit + offset}
 
+    if output == 'zip':
+
+        mem_file = BytesIO()
+        zip_file = zipfile.ZipFile(mem_file, 'w', zipfile.ZIP_DEFLATED)
+        current_time = datetime.now().strftime("%G_%m_%d")
+        file_name = 'berries_' + str(current_time) + '.json'
+        zip_file.writestr(file_name, r.content)
+
+        zip_file.close()
+
+        return StreamingResponse(
+            iter([mem_file.getvalue()]),
+            media_type="application/x-zip-compressed",
+            headers={"Content-Disposition": f"attachment;filename=download.zip"}
+        )
+
+    return {"error": "output not supported"}
